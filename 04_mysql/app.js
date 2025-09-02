e0b3153dc965a410e6156aa94f83bd38a7a5ee81
heochangbeom <heoc4869@gmail.com>
2025-09-02 17:45:06 +0900

    0902

--- 04_mysql/app.js
+++ 04_mysql/app.js
@@ -3,18 +3,48 @@ const express = require("express");
 const parser = require("body-parser");
 const sql = require("./sql");
 const prodSql = require("./sql/sql");
-const cors = require("cors");
+const cors = require("cors"); //허용
+const fs = require("fs");
 // {productList:{query:``}, productDetail:{}}
 
-console.log(prodSql["productMainImages"].query);
+// console.log(prodSql["productMainImages"].query);
 
 const app = express();
 app.use(parser.urlencoded()); // x-www-form-urlencoded
-app.use(parser.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
 app.get("/", (req, resp) => {
   resp.send("/ 실행");
+});
+
+app.post("/upload/:file_name", (req, resp) => {
+  let file_name = req.params.file_name;
+  let data = req.body.param;
+  console.log(file_name);
+  console.log(data);
+  resp.send("OK");
+  fs.writeFile(__dirname + "/uploads/" + file_name, data, "base64", (err) => {
+    if (err) {
+      resp.send(err);
+      return;
+    }
+    resp.send("ok");
+  });
+});
+
+// 이미지 링크정보.
+app.get("/download/:product_id/:path", (req, resp) => {
+  let product_id = req.params.product_id;
+  let path = req.params.path; //keyboard.jpg image/jpg
+  resp.header("Content-Type", `image/${path.substring(path.lastIndexOf("."))}`);
+  const filepath = `${__dirname}/uploads/${product_id}/${path}`;
+
+  if (!fs.existsSync(filepath)) {
+    resp.send("no image");
+    return;
+  }
+  fs.createReadStream(filepath).pipe(resp);
 });
 
 //상품쿼리.
